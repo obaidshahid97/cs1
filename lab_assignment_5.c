e3aaaf62c3f45c1f25053bfa9468bc50752a1c14
obaidshahid97 <69281785+obaidshahid97@users.noreply.github.com>
2023-06-17 20:29:06 -0400

    Add files via upload

---
+++ lab_assignment_5.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    char letter;
+    struct node* next;
+} node;
+
+int length(node* head) {
+    int count = 0;
+    node* current = head;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+char* toCString(node* head) {
+    int len = length(head);
+    char* res = malloc((len + 1) * sizeof(char));
+
+    int i = 0;
+    node* current = head;
+    while (current != NULL) {
+        res[i] = current->letter;
+        current = current->next;
+        i++;
+    }
+    res[len] = '\0';
+    return res;
+}
+
+void insertChar(node** pHead, char c) {
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->letter = c;
+    newNode->next = NULL;
+
+    if (*pHead == NULL) {
+        *pHead = newNode;
+    } else {
+        node* current = *pHead;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+void deleteList(node** pHead) {
+    node* current = *pHead;
+    while (current != NULL) {
+        node* next = current->next;
+        free(current);
+        current = next;
+    }
+    *pHead = NULL;
+}
+
+int main(void) {
+    int i, strLen, numInputs;
+    node* head = NULL;
+    char* str;
+    char c;
+    FILE* inFile = fopen("input.txt", "r");
+    fscanf(inFile, " %d\n", &numInputs);
+    while (numInputs-- > 0) {
+        fscanf(inFile, " %d\n", &strLen);
+        for (i = 0; i < strLen; i++) {
+            fscanf(inFile, " %c", &c);
+            insertChar(&head, c);
+        }
+        str = toCString(head);
+        printf("String is: %s\n", str);
+        free(str);
+        deleteList(&head);
+        if (head != NULL) {
+            printf("deleteList is not correct!");
+            break;
+        }
+    }
+    fclose(inFile);
+
+}
